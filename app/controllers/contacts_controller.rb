9c0255063093ecb6a652bf3c091432d5ae3f5c7f
Gordon Evans <gjrevans@gmail.com>
2017-06-04 21:10:13 -0400

    Added some error handling for Gibbon. Fixed some of the CSS.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,9 +4,20 @@ class ContactsController < ApplicationController
         gibbon = Gibbon::Request.new(api_key: ENV['MAILCHIMP_API_KEY'], debug: true)
         gibbon.timeout = 30
 
-        gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: params[:email], status: "pending", merge_fields: {FNAME: params[:first_name], LNAME: params[:last_name]}})
+        #If we're missing date redirect and show an error
+        if params[:email].blank? || params[:first_name].blank? || params[:last_name].blank?
+            flash[:danger] = t('alerts.subscribe_failure_missing_fields')
+            return redirect_to root_path
+        end
 
-        flash[:success] = t('alerts.subscribe_success')
-        redirect_to root_path
+        #Create the user and catch errors
+        begin
+            gibbon.lists(ENV['MAILCHIMP_LIST_ID']).members.create(body: {email_address: params[:email], status: "pending", merge_fields: {FNAME: params[:first_name], LNAME: params[:last_name]}})
+            flash[:success] = t('alerts.subscribe_success')
+            redirect_to root_path
+        rescue Gibbon::MailChimpError => error
+            flash[:success] = error.detail
+            redirect_to root_path
+        end
     end
 end
