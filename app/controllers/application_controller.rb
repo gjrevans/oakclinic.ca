36727610d191a15907f2b297cc15be158252bd34
Gordon Evans <gjrevans@gmail.com>
2017-06-11 21:18:17 -0400

    Got rid of locales for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :set_locale
+    #before_action :set_locale
 
     private
     def set_locale
         I18n.locale = params[:locale].present? ? params[:locale] : "en"
-    end
-
-    def default_url_options(options = {})
-        {locale: I18n.locale}
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-    scope "(:locale)", locale: /en|fr/ do
-        #User Routes
-        devise_for :users
+    # User Routes
+    devise_for :users
 
-        #Page Routes
-        root to: 'pages#index'
-        get 'about', to: 'pages#about', as: 'about'
-        get 'naturopathic_medicine', to: 'pages#naturopathic_medicine', as: 'naturopathic_medicine'
+    # Page Routes
+    root to: 'pages#index'
+    get 'about', to: 'pages#about', as: 'about'
+    get 'naturopathic_medicine', to: 'pages#naturopathic_medicine', as: 'naturopathic_medicine'
 
-        #Mailer Routes
-        post 'create_contact', to: 'contacts#create', as: 'create_contact'
-    end
+    # Mailer Routes
+    post 'create_contact', to: 'contacts#create', as: 'create_contact'
 end
