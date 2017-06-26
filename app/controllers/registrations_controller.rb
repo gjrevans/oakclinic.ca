0c994e1efbd8d91bf40681e91ef3123242752da3
Gordon Evans <gjrevans@gmail.com>
2017-06-25 22:32:52 -0400

    Disable registrations.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,21 @@
 class RegistrationsController < Devise::RegistrationsController
-  private
+    def new
+      flash[:danger] = 'Silly rabbit, registrations are for kids!'
+      redirect_to root_path
+    end
 
-  def sign_up_params
-    params.require(:user).permit(:first_name, :last_name, :image, :email, :password, :password_confirmation)
-  end
+    def create
+      flash[:danger] = 'Silly rabbit, registrations are for kids!'
+      redirect_to root_path
+    end
 
-  def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :image, :email, :password, :password_confirmation, :current_password)
-  end
+    private
+
+    def sign_up_params
+        params.require(:user).permit(:first_name, :last_name, :image, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permit(:first_name, :last_name, :image, :email, :password, :password_confirmation, :current_password)
+    end
 end
